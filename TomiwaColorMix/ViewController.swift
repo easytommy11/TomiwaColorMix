ebebd02bbfc9b540572b20bd47200a9135314ac9
Tomiwa Babalola <tbabalola004@gmail.com>
2021-11-23 09:27:22 -0500

    bksnddk

--- TomiwaColorMix/ViewController.swift
+++ TomiwaColorMix/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redSwitch: UISwitch!
+    @IBOutlet var greenSwitch: UISwitch!
+    @IBOutlet var blueSwitch: UISwitch!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    func updateColor() {
+    var red: CGFloat = 0
+    var green: CGFloat = 0
+    var blue: CGFloat = 0
+    if redSwitch.isOn {
+        red = 1
+    }
+    if greenSwitch.isOn {
+    green = 1
+    }
+    if blueSwitch.isOn {
+    blue = 1
+    }
+    let color = UIColor(red: red, green: green, blue: blue, alpha:1)
+    colorView.backgroundColor = color
+    }
+
+    
     override func viewDidLoad() {
+        colorView.backgroundColor = .black
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func sliderChanged(_ sender: Any) {
+    }
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+        colorView.backgroundColor = .red
+        }
+        else {
+        colorView.backgroundColor = .black
+        }
+
+    }
 }
 
