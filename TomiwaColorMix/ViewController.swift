32cbb79d5ab3786fba4016a4782994c0278bc823
Tomiwa Babalola <tbabalola004@gmail.com>
2021-11-24 09:05:19 -0500

    jdhsjnfcc

--- TomiwaColorMix/ViewController.swift
+++ TomiwaColorMix/ViewController.swift
@@ -21,17 +21,22 @@ class ViewController: UIViewController {
     var red: CGFloat = 0
     var green: CGFloat = 0
     var blue: CGFloat = 0
-    if redSwitch.isOn {
-        red = 1
-    }
-    if greenSwitch.isOn {
-    green = 1
-    }
-    if blueSwitch.isOn {
-    blue = 1
-    }
-    let color = UIColor(red: red, green: green, blue: blue, alpha:1)
-    colorView.backgroundColor = color
+        if redSwitch.isOn {
+            red = CGFloat(redSlider.value)
+        }
+        
+        if greenSwitch.isOn {
+            green = CGFloat(greenSlider.value)
+        }
+        
+        if blueSwitch.isOn {
+            blue = CGFloat(blueSlider.value)
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+
+
     }
 
     
@@ -43,15 +48,17 @@ class ViewController: UIViewController {
 
 
     @IBAction func sliderChanged(_ sender: Any) {
+        updateColor()
     }
+    
+
+
     @IBAction func switchChanged(_ sender: UISwitch) {
-        if sender.isOn {
-        colorView.backgroundColor = .red
-        }
-        else {
-        colorView.backgroundColor = .black
-        }
+       updateColor()
 
     }
+    
+    
+    
 }
 
